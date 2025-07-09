763d6d04ed60bb695f200f7a156c374675467233
ivy <ivy.chemutai@student.moringaschool.com>
2025-07-09 22:44:44 +0300

    react-app-ecxample

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -11,15 +11,15 @@ test('should include "Now" in the header instead of a time', () => {
   expect(
     screen.queryByText(format(new Date(), "MMMM do yyyy, h:mm:ss a"))
   ).not.toBeInTheDocument();
-  expect(screen.queryByText(/Now/g)).toBeInTheDocument();
+  expect(screen.queryByText(/Now/i)).toBeInTheDocument();
 });
 
 test("should include the <ExampleComponent />", () => {
-  expect(screen.queryByText("Whoa!")).toBeInTheDocument();
+  expect(screen.queryByText(/Whoa!/i)).toBeInTheDocument();
 });
 
 test("should include the <TestComponent />", () => {
-  expect(screen.queryByTitle("time video")).toBeInTheDocument();
+  expect(screen.queryByTitle(/time video/i)).toBeInTheDocument();
 });
 
 //   it('should include "Now" in the header instead of a time', () => {
--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+return(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+export default App;
